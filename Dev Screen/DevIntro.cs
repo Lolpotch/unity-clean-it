620d5a75e6aee18a38d5999e7428af4ed72616d7
Chandra Tritaqwa Ramadhan <107376090+Lolpotch@users.noreply.github.com>
2024-08-31 13:18:42 +0700

    Add files via upload

---
+++ Dev Screen/DevIntro.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class DevIntro : MonoBehaviour
+{
+    public void LoadStartScene()
+    {
+        FindObjectOfType<GameManager>().LoadScene("Title Screen");
+    }
+}
