48bcf9ecef7b82d290431f29ee9de4364e6f31c2
Chandra Tritaqwa Ramadhan <107376090+Lolpotch@users.noreply.github.com>
2024-08-31 15:31:52 +0700

    Add files via upload

---
+++ Assets/Score.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    CounterManager counter;
+    [SerializeField] Text[] scores;
+    int[] amount;
+
+    private void Awake()
+    {
+        counter = FindObjectOfType<CounterManager>();
+
+        amount = new int[3];
+        amount[0] = counter.coinAmount;
+        amount[1] = counter.givenMaskAmount;
+        amount[2] = counter.disinfectedObjectsAmount;
+    }
+
+    void Start()
+    {
+        for(int i = 0; i < scores.Length; i++)
+        {
+            scores[i].text = amount[i].ToString() + scores[i].text;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
